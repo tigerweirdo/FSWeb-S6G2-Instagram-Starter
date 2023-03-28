c21caa16b8c6d99943647613f36429d258a2c615
tigerweirdo <temmuzcetiner@gmail.com>
2023-03-28 22:24:34 +0200

    bitti.

--- src/bileşenler/Gönderiler/Gönderiler.js
+++ src/bileşenler/Gönderiler/Gönderiler.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import Gönderi from './Gönderi';
-import './Gönderiler.css';
+import React from "react";
+import Gönderi from "./Gönderi";
+import "./Gönderiler.css";
 
 const Gönderiler = (props) => {
   // 🔥 Gönderiler'in ebeveyninin doğru değişkenleri doğru şekilde ilettiğine emin olun!
   const { gonderiyiBegen, gonderiler } = props;
 
   return (
-    <div className='posts-container-wrapper'>
-      {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/
-      gonderiler.map( g => (
-          <Gönderi gönderi={g} gonderiyiBegen={gonderiyiBegen} key={g.id}/>
-      ))
-      }
-      {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
+    <div className="posts-container-wrapper">
+      {" "}
+      {gonderiler.map((x) => (
+        <Gönderi gönderi={x} gonderiyiBegen={gonderiyiBegen} key={x.id} />
+      ))}
     </div>
   );
 };
